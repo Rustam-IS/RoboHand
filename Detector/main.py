51521ad5d9ceaa3a65ac70330e821be07f0f3f87
Rustam <106992439+Rustam-IS@users.noreply.github.com>
2023-01-19 18:43:35 +0300

    Add files via upload

--- Detector/main.py
+++ Detector/main.py
@@ -1,10 +1,21 @@
+def scale(x, in_min, in_max, out_min, out_max):
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+
+
 import cv2
 import mediapipe as mp
+from serial import Serial
+
 
 cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
-hands = mpHands.Hands()
+hands = mpHands.Hands(max_num_hands=4)
 mpDraw = mp.solutions.drawing_utils
+
+serial = Serial(
+    port="COM29",
+    baudrate=115200,
+)
 
 while True:
     success, image = cap.read()
@@ -18,8 +29,17 @@ while True:
                 h, w, c = image.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
 
-                if id == 20:
+                if id == 8:
+                    a = lm.y
                     cv2.circle(image, (cx, cy), 25, (255, 0, 255), cv2.FILLED)
+
+                if id == 5:
+                    b = lm.y
+                    cv2.circle(image, (cx, cy), 25, (0, 0, 255), cv2.FILLED)
+
+            if 0.04 < b - a < 0.2:
+                serial.write(bytes([2]))
+                serial.write(bytes([int(scale(b - a, 0.04, 0.2, 255, 0))]))  # 0.04 0.2 -> 255 0
 
             mpDraw.draw_landmarks(image, handLms, mpHands.HAND_CONNECTIONS)
 
